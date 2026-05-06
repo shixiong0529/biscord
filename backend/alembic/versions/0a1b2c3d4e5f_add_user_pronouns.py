edc0b8532bed89143fab6ab661d1e0708104e298
shixiong <drinkpig@outlook.com>
2026-05-06 19:25:58 +0800

    Fix pronouns migration parent

--- backend/alembic/versions/0a1b2c3d4e5f_add_user_pronouns.py
+++ backend/alembic/versions/0a1b2c3d4e5f_add_user_pronouns.py
@@ -1,7 +1,7 @@
 """add user pronouns
 
 Revision ID: 0a1b2c3d4e5f
-Revises: f6a7b8c9d0e1
+Revises: 718a9e58829e
 Create Date: 2026-05-06 00:00:00.000000
 """
 from typing import Sequence, Union
@@ -9,7 +9,7 @@ from alembic import op
 import sqlalchemy as sa
 
 revision: str = '0a1b2c3d4e5f'
-down_revision: Union[str, None] = 'f6a7b8c9d0e1'
+down_revision: Union[str, None] = '718a9e58829e'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
